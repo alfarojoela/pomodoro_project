90ec2dcd9a9542758784b4ed91d26162e3cdad0e
Joel Alfaro <alfarojoela@outlook.com>
2021-10-11 16:01:17 -0700

    count down mechanism started

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -13,9 +14,20 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-import time
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count-1)
+
 
 
 
@@ -28,6 +40,7 @@ import time
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
+
 
 #fg argument sets color foreground for label text
 
@@ -43,12 +56,15 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0) #last ar
 
 tomato_img= PhotoImage(file="tomato.png")  #this function needs file path
 canvas.create_image(100, 112, image=tomato_img) #x position is 100, y position is 112 image as argument points to variable name
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold")) #default text, color is white
+timer_text=canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold")) #default text, color is white
 #FONT_NAME is above constant
 
 canvas.grid(row=1, column =1)  #appears to center canvas.
 
-start_button = Button(text="Start",highlightthickness=0, font=(FONT_NAME, 12))
+#count_down(5)
+
+#command needs to be set to the name of the function to trigger
+start_button = Button(text="Start",highlightthickness=0, font=(FONT_NAME, 12), command=start_timer)
 start_button.grid(column=0, row=2) #original row value was 5. doesn't seem much different
 
 reset_button = Button(text="Reset",highlightthickness=0, font=(FONT_NAME, 12))
