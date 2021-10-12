9a1c2b56b35d7f73666fd3c1932510d8a1bc9ea2
Joel Alfaro <alfarojoela@outlook.com>
2021-10-11 17:17:01 -0700

    finished.  need to review and comment.

--- main.py
+++ main.py
@@ -11,22 +11,68 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+reps= 0
+timer = None
+
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
+
+
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+    #if it's the 8th rep:
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Break", fg=RED)
+
+     #if it's the 2nd/4th/6th rep:
+    elif reps %2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Break", fg=PINK)
+
+    #if it's the 1st/3rd/5th/7th rep:
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
 
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count-1)
+        global timer
+        timer = window.after(1000, count_down, count-1)
+    else:
+        start_timer()
+        mark ="✓"
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            mark += ""
+        check_marks.config(text=mark)
 
 
 
@@ -67,10 +113,10 @@ canvas.grid(row=1, column =1)  #appears to center canvas.
 start_button = Button(text="Start",highlightthickness=0, font=(FONT_NAME, 12), command=start_timer)
 start_button.grid(column=0, row=2) #original row value was 5. doesn't seem much different
 
-reset_button = Button(text="Reset",highlightthickness=0, font=(FONT_NAME, 12))
+reset_button = Button(text="Reset",highlightthickness=0, command=reset_timer, font=(FONT_NAME, 12))
 reset_button.grid(column = 3, row=2)
 
-check_marks = Label(text="✓", fg=GREEN, bg=YELLOW, font=(100))
+check_marks = Label(text="", fg=GREEN, bg=YELLOW, font=(100))
 check_marks.grid(column=1, row=3)
 
 
